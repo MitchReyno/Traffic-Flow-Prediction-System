1489590eec0f32b8e9d75b300c5753b9e9bc9ced
Jake Scott <coolasjake8@gmail.com>
2019-10-06 23:29:17 +1100

    Nodes are now aligned with the map, and code is more robust

--- TFPS/create_map_ui.py
+++ TFPS/create_map_ui.py
@@ -13,6 +13,9 @@ ORANGE = (255, 125, 0)
 YELLOW = (255, 255, 0)
 BROWN = (90, 65, 50)
 
+OPTN_W = 200
+SCRN_H = 790
+SCRN_W = int((0.82484 * SCRN_H)) + OPTN_W
 
 class CardinalDir:
     @staticmethod
@@ -73,15 +76,32 @@ class CardinalDir:
             return {-1, 1}
 
     @staticmethod
-    def pos_to_screen(pos, screen_width=1300, screen_height=700):
+    def ORIGINAL_pos_to_screen(pos, screen_width=SCRN_W, screen_height=SCRN_H, options_width=OPTN_W):
+        offset_lat = -0.0016827
+        offset_long = -0.0013543
+        min_lat = -37.8676
+        max_lat = -37.78093
+        min_long = 145.0083
+        max_long = 145.09885
+        lat_offset = 0.000
+        long_offset = 0.000
+        lat_space = max_lat - min_lat
+        long_space = max_long - min_long
+        screen_x = ((pos[0] + lat_offset - min_lat) / lat_space) * (screen_width - options_width) + options_width
+        screen_y = ((pos[1] + long_offset - min_long) / long_space) * screen_height
+        screen_pos = [int(screen_x), int(screen_y)]
+        return screen_pos
+
+    @staticmethod
+    def pos_to_screen(pos, screen_width=SCRN_W, screen_height=SCRN_H, options_width=OPTN_W):
         min_lat = -37.8676
         max_lat = -37.78093
         min_long = 145.0083
         max_long = 145.09885
         lat_space = max_lat - min_lat
         long_space = max_long - min_long
-        screen_x = ((pos[0] - min_lat) / lat_space) * screen_width
-        screen_y = ((pos[1] - min_long) / long_space) * screen_height
+        screen_x = ((pos[1] - min_long) / long_space) * (screen_width - options_width) + options_width
+        screen_y = screen_height - (((pos[0] - min_lat) / lat_space) * screen_height)
         screen_pos = [int(screen_x), int(screen_y)]
         return screen_pos
 
@@ -165,18 +185,57 @@ def get_node_data(print_raw, print_formatted):
     return formatted_nodes
 
 
-def draw_common(screen, nodes):
+def draw_common(screen, nodes, map_img, map_offset):
     screen.fill(WHITE)
+    screen.blit(map_img, (OPTN_W + map_offset[0], map_offset[1]))
     node_rep_size = 5
     for node in nodes:
-        print(CardinalDir.pos_to_screen(node.avg_pos))
         screen_pos = CardinalDir.pos_to_screen(node.avg_pos)
         pygame.draw.circle(screen, GREEN, screen_pos, node_rep_size, 1)
+        for scat in node.directions:
+            screen_pos = CardinalDir.pos_to_screen(scat.pos)
+            pygame.draw.circle(screen, BLUE, screen_pos, node_rep_size, 1)
+
+
+def draw_in_info_mode(screen, nodes, map_img, mouse_pos, map_offset):
+    draw_common(screen, nodes, map_img, map_offset)
+    closest = 1000
+    chosen_scat = nodes[0]
+    for node in nodes:
+        screen_pos = CardinalDir.pos_to_screen(node.avg_pos)
+        dist = math.sqrt(math.pow(abs(mouse_pos[0] - screen_pos[0]), 2) +
+                         math.pow(abs(mouse_pos[1] - screen_pos[1]), 2))
+        if dist < 5:
+            chosen_scat = node
+            break
+        elif dist < closest:
+            chosen_scat = node
+            closest = dist
+    screen_pos = CardinalDir.pos_to_screen(chosen_scat.avg_pos)
+    pygame.draw.circle(screen, RED, screen_pos, 15, 1)
+    closest = 1000
+    chosen_node = chosen_scat.directions[0]
+    for scat in chosen_scat.directions:
+        screen_pos = CardinalDir.pos_to_screen(scat.pos)
+        dist = math.sqrt(math.pow(abs(mouse_pos[0] - screen_pos[0]), 2) +
+                         math.pow(abs(mouse_pos[1] - screen_pos[1]), 2))
+        if dist < closest:
+            chosen_node = scat
+            closest = dist
+
+    font = pygame.font.Font('freesansbold.ttf', 12)
+    text = font.render(chosen_node.name, True, BLACK, YELLOW)
+    text_rect = text.get_rect()
+    text_rect.center = CardinalDir.pos_to_screen(chosen_node.pos)
+    screen.blit(text, text_rect)
+    text = font.render("Lat: {0}, Long: {1}".format(chosen_node.pos[0], chosen_node.pos[1]), True, BLACK, ORANGE)
+    text_rect = text.get_rect()
+    coords_pos = CardinalDir.pos_to_screen(chosen_node.pos)
+    coords_pos[1] += 12
+    text_rect.center = coords_pos
+    screen.blit(text, text_rect)
+
     pygame.display.flip()
-
-
-def draw_in_info_mode(screen, nodes, mouse_pos):
-    draw_common(screen, nodes)
 
 
 def draw_in_select_mode():
@@ -196,22 +255,44 @@ def draw_in_speed_mode():
 
 
 def start_input_loop():
-    size = [1300, 700]
+    size = [SCRN_W  + int((SCRN_W * 0.1)), SCRN_H]
     screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('TFPS Map Viewer')
     nodes = get_node_data(False, True)
-    draw_in_info_mode(screen, nodes, [0, 0])
+    map_img = pygame.image.load("data/final_map.png")
+
+    #draw_in_info_mode(screen, nodes, map_img, [0, 0])
 
     mode = "info"
+    map_offset = [-154, 0]
     done = False
     while not done:
+        keys = pygame.key.get_pressed()
+        mods = pygame.key.get_mods()
+
+        if False:  # Change to true to allow map adjustment
+            speed = 1
+            if mods & pygame.KMOD_LSHIFT:
+                speed = 10
+            if keys[pygame.K_LEFT]:
+                map_offset[0] -= speed
+            if keys[pygame.K_RIGHT]:
+                map_offset[0] += speed
+            if keys[pygame.K_UP]:
+                map_offset[1] += speed
+            if keys[pygame.K_DOWN]:
+                map_offset[1] -= speed
+
+            if keys[pygame.K_SPACE]:
+                print("map offset: ({0}, {1})".format(map_offset[0], map_offset[1]))
+
         mouse_event = 0
-        mouse_pos = {-1, -1}
+        mouse_pos = pygame.mouse.get_pos()
         for event in pygame.event.get():  # User did something
             if event.type == pygame.QUIT:  # If user clicked close
                 done = True  # Flag that we are done so we exit this loop
             elif event.type == pygame.MOUSEBUTTONUP:
-                mouse_pos = pygame.mouse.get_pos()
-                if mouse_pos[0] < 260:
+                if mouse_pos[0] < OPTN_W:
                     mouse_event = 1
                 else:
                     mouse_event = 2
@@ -238,6 +319,7 @@ def start_input_loop():
         if mouse_event == 2:  # Map clicked
             print("Clicked Map at: (x={0}, y={1})".format(mouse_pos[0], mouse_pos[1]))
 
+        draw_in_info_mode(screen, nodes, map_img, mouse_pos, map_offset)
 
 
 start_input_loop()
